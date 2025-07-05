1514049a31bca427a697f33b6538198baa52d10a
SonkarXKoushal <sonkarp463@gmail.com>
2025-07-05 19:09:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SonkarXKoushal/mahii",
+    "https://github.com/SonkarXKoushal/star",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
